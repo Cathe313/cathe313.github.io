857d2edab126c44ed8bfcbaa2b9633639dbeee19
Catherine <ducharme.catherine@gmail.com>
2015-11-07 21:46:53 +0000

    Installed grunt-webpack, created Backbone router

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
 $(document).foundation();
+
+var Backbone = require('backbone');
+
+var router = Backbone.Router.extend({
+    routes: {
+        '': 'homePage',
+        'main': 'main',
+        'portfolio': 'portfolio',
+        'contact': 'contact'
+    },
+    homePage: function() {
+        this.navigate('main', {trigger: true});
+    },
+    main: function() {
+        console.log('main');
+    },
+    portfolio: function() {
+        console.log('portfolio');
+    },
+    contact: function() {
+        console.log('contact');
+    }
+});
+
+var thisRouter = new router;
+
+thisRouter.on();
+
+Backbone.history.start();
